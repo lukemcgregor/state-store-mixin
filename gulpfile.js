fc37d0ea81d3d8ab34ff2c999bf1d94d60326ca5
Luke McGregor <luke@mcgregor.org.nz>
2015-06-11 19:06:24 +1200

    gulps

--- gulpfile.js
+++ gulpfile.js
@@ -13,45 +13,45 @@ const mkdirp = require('mkdirp');
 const fs = require('fs');
 const path = require('path');
 
-// add custom browserify options here
-var customOpts = {
-  entries: ['./example/src/app.js'],
-  debug: true
-};
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts));
-
-// add transformations here
-b.transform(browserifyShim);
-
-gulp.task('dev-watch', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
-
-function bundle() {
-  return b.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('app.js'))
-    // optional, remove if you don't need to buffer file contents
-    .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
-    .pipe(sourcemaps.write('./')) // writes .map file
-    .pipe(gulp.dest('./example/scripts/'));
-}
+// // add custom browserify options here
+// var customOpts = {
+//   entries: ['./example/src/app.js'],
+//   debug: true
+// };
+// var opts = assign({}, watchify.args, customOpts);
+// var b = watchify(browserify(opts));
+//
+// // add transformations here
+// b.transform(browserifyShim);
+//
+// gulp.task('dev-watch', bundle); // so you can run `gulp js` to build the file
+// b.on('update', bundle); // on any dep update, runs the bundler
+// b.on('log', gutil.log); // output build logs to terminal
+//
+// function bundle() {
+//   return b.bundle()
+//     // log errors if they happen
+//     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+//     .pipe(source('app.js'))
+//     // optional, remove if you don't need to buffer file contents
+//     .pipe(buffer())
+//     // optional, remove if you dont want sourcemaps
+//     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+//        // Add transformation tasks to the pipeline here.
+//     .pipe(sourcemaps.write('./')) // writes .map file
+//     .pipe(gulp.dest('./example/scripts/'));
+// }
 
 
 gulp.task("dist", function (done) {
-  var exportFileName = 'state-store';
-  var exportVarName = 'stateStore';
-  var entryFileName = 'state-store';
+  var exportFileName = 'state-store-mixin';
+  var exportVarName = 'stateStoreMixin';
+  var entryFileName = 'state-store-mixin';
   var destinationFolder = 'dist'
 
   esperanto.bundle({
     base: 'src',
-    entry: 'state-store',
+    entry: entryFileName,
   }).then(function(bundle) {
     var res = bundle.toUmd({
       sourceMap: true,
